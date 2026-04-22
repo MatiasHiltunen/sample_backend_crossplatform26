f3d78c5d8d1c7881406f942c1969bf81fda3f73e
matiashiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2026-04-22 21:16:13 +0300

    update deprecated fastapi feature

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt import InvalidTokenError
 from pwdlib import PasswordHash
 from pydantic import BaseModel
+from contextlib import asynccontextmanager
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -15,6 +16,14 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 DB_PATH = "app.db"
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+
 
 
 class Token(BaseModel):
@@ -50,7 +59,7 @@ DUMMY_HASH = password_hash.hash("dummypassword")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 def verify_password(plain_password, hashed_password):
@@ -227,6 +236,3 @@ def init_db():
         )
 
 
-@app.on_event("startup")
-def on_startup():
-    init_db()
